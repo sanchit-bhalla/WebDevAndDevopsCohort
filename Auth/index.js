6b2c3e6cf432d7eca6d585b6fb072fdc3697b183
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2024-09-28 18:23:39 +0530

    Add authenticated route /me

--- Auth/index.js
+++ Auth/index.js
@@ -52,6 +52,21 @@ app.post("/signin", (req, res) => {
     }
 })
 
+app.get("/me", (req, res) => {
+    const token = req.headers.authorization;
+    const user = users.find(user => user.token === token)
+
+    if(user){
+        res.send({
+            username: user.username
+        })
+    } else{
+        res.status(401).send({
+            message: "You are not authorized!"
+        })
+    }
+})
+
 app.listen(port, (err) => {
     console.log('server is runnig on port: ' + port)
 })
