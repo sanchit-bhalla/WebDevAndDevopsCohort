aae00dbac5016037713059ce78f6df564de90d33
Sanchit Bhalla <sanchitbhalla91@gmail.com>
2025-02-23 12:45:00 +0530

    Link publish and unpublish feature added

--- second-brain/brainly-backend/src/controllers/brain.controller.ts
+++ second-brain/brainly-backend/src/controllers/brain.controller.ts
@@ -3,6 +3,8 @@ import { Link } from "../models/link.model";
 import { ApiResponse } from "../utils/ApiResponse";
 import { asyncHandler } from "../utils/asyncHandler";
 import { HASH_SECRET } from "../config";
+import { ApiError } from "../utils/ApiError";
+import { Content } from "../models/content.model";
 
 const generateUniqueHash = function (userId: string) {
   const hash = crypto.createHmac("sha256", HASH_SECRET as string);
@@ -30,6 +32,7 @@ export const shareBrain = asyncHandler(async (req, res, next) => {
 
     return res.status(200).json(new ApiResponse(200, { hash }));
   } else {
+    // unpublish the Link
     await Link.deleteOne({
       userId: req?.user?._id,
     });
@@ -38,4 +41,18 @@ export const shareBrain = asyncHandler(async (req, res, next) => {
   }
 });
 
-export const brainContent = asyncHandler(async (req, res, next) => {});
+export const brainContent = asyncHandler(async (req, res, next) => {
+  const hash = req.params.shareLink;
+
+  const link = await Link.findOne({
+    hash,
+  });
+  if (!link) throw new ApiError(404, "URL not exist");
+
+  // If link exists, get userId from it and return the brain of that user
+  const contents = await Content.find({
+    userId: link.userId,
+  });
+
+  return res.status(200).json(new ApiResponse(200, contents));
+});
